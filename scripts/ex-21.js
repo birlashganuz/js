98a1607e002a89d9c95fe3418715bae289b4e60e
birlashganuz <birlashganuz@gmail.com>
2026-03-23 16:22:08 +0500

    v2+

--- scripts/ex-21.js
+++ scripts/ex-21.js
@@ -4,12 +4,11 @@ function dectobin(k) {
   while (k >= bin) {
     result = String(k % bin) + result;
     k = (k - (k % bin)) / bin;
-
-    console.log(k);
   }
   result = String(k) + result;
   return result;
 }
 
 k = +prompt("Son kiriting:");
-console.log(dectobin(k));
+console.log("Berilgan son: " + k);
+console.log("Berilgan sonnig ikkilikdagi ko'rinishi: " + dectobin(k));
